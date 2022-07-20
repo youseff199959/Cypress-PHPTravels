f763f9d76bf3f340150593cb0aa88d0c3c563002
Yousef Mahfouz <youseff199959@gmail.com>
2022-07-20 18:24:11 +0200

    login with invalid data commit 2

--- cypress/e2e/1-getting-started/3-login_inValidData.cy.js
+++ cypress/e2e/1-getting-started/3-login_inValidData.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 //importing the data file that has all the locators and the required data for the test cases 
+import { faker } from '@faker-js/faker';
 const data = require('../data/data.js');
 const loginData = require('../data/inValidLoginData')
 
@@ -15,8 +16,20 @@ describe('Open the required website', () => {
             cy.get(data.elements.login.emailField).type(email,{force: true})
             cy.get(data.elements.login.passwordField).type(password,{force: true})
             cy.get(data.elements.login.loginFormButton).click({force: true})
-            cy.get('form[method="POST"][action="https://www.phptravels.net/login"]').should('contain','Please fill out this field')
+            cy.title().should('eq','Login - PHPTRAVELS')
           })
+
+    })
+
+    it("Login with invalid Email and Password",()=>{
+  
+        cy.get(data.elements.login.loginButton).should('be.visible').click();
+        cy.title().should('eq','Login - PHPTRAVELS')
+        cy.get(data.elements.login.emailField).type(faker.internet.email(),{force: true})
+        cy.get(data.elements.login.passwordField).type(faker.random.alpha(15),{force: true})
+        cy.get(data.elements.login.loginFormButton).click({force: true})
+        cy.get(data.elements.login.loginFailed).should('contain.text',' Wrong credentials. try again')
+        cy.title().should('eq','Login - PHPTRAVELS')
 
     })
     
--- cypress/e2e/data/inValidLoginData.js
+++ cypress/e2e/data/inValidLoginData.js
@@ -2,11 +2,6 @@ import { faker } from '@faker-js/faker';
 
 module.exports=[
     {
-        testCase : "Login with wrong email and password ",
-        email: faker.internet.email(),
-        password: faker.random.alpha(15),
-    },
-    {
         testCase : "Login with wrong email and empty password ",
         email: faker.internet.email(),
         password: "{backspace}",
